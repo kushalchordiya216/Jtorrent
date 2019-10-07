18e97f511b4eda8ec248b185daaf1dac2b19d7da
chetanatole <31009167+chetanatole@users.noreply.github.com>
2019-10-07 22:45:09 +0530

    Updated Decode
    Decode constructor needs Path of the metadata file and the username. Need updates in peer.java

--- src/main/java/jtorrent/Encoding/Decode.java
+++ src/main/java/jtorrent/Encoding/Decode.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 
 public class Decode {
     private HashMap<String, String> metaDataHash;
-
+    private String username;
     // TODO: Read from proper path
     // TODO: Create copy of metadata in proper path
-    public Decode(String metaFile) {
-        File metaFile1 = new File(metaFile);
+    public Decode(String metaFilePath,String username) {
+    	this.username = username;
+    	metaDataHash = new HashMap<String, String>();
+        File metaFile1 = new File(metaFilePath);
         ObjectInputStream ois;
         try {
             ois = new ObjectInputStream(new FileInputStream(metaFile1));
@@ -26,11 +28,11 @@ public class Decode {
     }
 
     public void Merge() {
-        File file = new File(this.metaDataHash.get("Name"));
+        File file = new File(System.getProperty("user.home") + "/Downloads/" + this.metaDataHash.get("Name"));
         OutputStream outputStream;
         try {
-            outputStream = new FileOutputStream(file);
-            for (int i = 0; i < metaDataHash.size(); i++) {
+            outputStream = new FileOutputStream(file);    
+            for (int i = 0; i < metaDataHash.size()-3; i++) {
                 byte[] content = read(this.metaDataHash.get(Integer.toString(i)));
                 outputStream.write(content);
                 outputStream.flush();
@@ -46,9 +48,9 @@ public class Decode {
     }
 
     private byte[] read(String fileName) {
-        byte[] pieceContent = new byte[1024 * 1024];
-        File piece = new File(fileName);
-        InputStream inputStream;
+        File piece = new File(System.getProperty("user.home") + "/.P2P/" + username + "/" + getMerkleRoot() + "/" +fileName);
+        byte[] pieceContent = new byte[(int) piece.length()];
+        FileInputStream inputStream;
         try {
             inputStream = new FileInputStream(piece);
             inputStream.read(pieceContent);
@@ -58,4 +60,9 @@ public class Decode {
         }
         return pieceContent;
     }
+//    public static void main(String args[])
+//    {
+//    	Decode ob = new Decode("/home/chetan/.P2P/Chetan/c5a50472f73cc1b3cfc643494369622b62099707e7be52c62d6a5b3b34812b1a/a1.txt.metadata","Chetan");
+//    	ob.Merge();
+//    }
 }
