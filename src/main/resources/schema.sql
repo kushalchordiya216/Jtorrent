e033e178ca33295f3728bd9ab5e1f57f383acb78
Kushal Chordiya <chordiyakushal@gmail.com>
2019-10-20 02:11:54 +0530

    minor db change

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,12 +19,12 @@ CREATE TABLE Users
 CREATE TABLE Files
 (
     merkleRoot VARCHAR(256) NOT NULL,
-    filename VARCHAR(100) NOT NULL,
+    filename VARCHAR(100) DEFAULT NULL,
     fiesizeMB INTEGER(10) default NULL,
     username   VARCHAR(100) NOT NULL,
     currentIP  VARCHAR(100) NOT NULL,
     active tinyint NOT NULL,
     foreign key (username, currentIP) references Users (username, currentIP) on update cascade on delete cascade,
     foreign key (username, active) references Users(username, active) on update cascade on delete cascade,
-    primary key (username, merkleRoot)
+    unique key (username, merkleRoot)
 );
